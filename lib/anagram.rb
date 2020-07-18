f563b29a5dfe215a6680156d3c1f4a847418c91d
Adam Palmer <theadampalmer@gmail.com>
2020-07-18 23:32:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+ 
+  def initialize(word)
+    @word = word
+  end
+   
+  def match(wordlist)
+		anagram = []
+		
+		wordlist.each do |diff_word|
+			if @word.split("").sort == diff_word.split("").sort
+				anagram << diff_word
+			end
+	end
+		
+		anagram
+
+		wordlist.select do |diff_word|
+			@word.split("").sort == diff_word.split("").sort
+		end
+	end
+    
+end
