a996a51639df241efe2f015828db0f6f6e1cee76
Adam Palmer <theadampalmer@gmail.com>
2020-07-18 23:32:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,5 +7,21 @@ class Anagram
   def initialize(word)
     @word = word
   end
+   
+  def match(wordlist)
+		anagram = []
+		
+		wordlist.each do |diff_word|
+			if @word.split("").sort == diff_word.split("").sort
+				anagram << diff_word
+			end
+	end
+		
+		anagram
+
+		wordlist.select do |diff_word|
+			@word.split("").sort == diff_word.split("").sort
+		end
+	end
     
 end
